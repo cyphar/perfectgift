271874b7ddfeb9c06a74b4096be5cce70aa5b492
cyphar <cyphar@cyphar.com>
2014-01-18 04:24:32 +1100

    epyc: re-added comments

--- epyc/tokeniser.py
+++ epyc/tokeniser.py
@@ -21,7 +21,7 @@ def tokenise(template_string):
 		#print(upto, template_string[upto])
 		#iterate through until token controls
 		# looking for {{ or {%
-		if upto < len(template_string) -1 and (template_string[upto:upto+2] == '{{' or template_string[upto:upto+2] == '{%'):
+		if (upto < len(template_string)-1) and (template_string[upto:upto+2] in ['{{', '{%', '{#']):
 			if upto > 0 and token_start < upto - 1:
 				# add HTML token
 				tokens.append(template_string[token_start:upto])
@@ -30,20 +30,21 @@ def tokenise(template_string):
 
 			token = template_string[upto:upto+2]
 			#print('found token {0} at {1}'.format(token, upto))
-			token_start = upto
-			tokens.append(template_string[token_start:upto+2])
 			upto += 2
 			token_start = upto
-
 			if token == '{{':
 				upto = find_token(template_string, upto, '}}')
 			elif token == '{%':
 				upto = find_token(template_string, upto, '%}')
+			elif token == '{#':
+				upto = find_token(template_string, upto, '#}')
 
-			tokens.append(template_string[token_start:upto])
-			token = template_string[upto:upto+2]
-			#print('found token {0} at {1}'.format(token, upto))
-			tokens.append(template_string[upto:upto+2])
+			if token != '{#':
+				tokens.append(token)
+				tokens.append(template_string[token_start:upto])
+				token = template_string[upto:upto+2]
+				#print('found token {0} at {1}'.format(token, upto))
+				tokens.append(token)
 			upto += 2
 			token_start = upto
 		else:
@@ -56,14 +57,14 @@ def tokenise(template_string):
 	return tokens
 
 if __name__ == "__main__":
-	def test_tokeniser(string):
-		tokens = tokeniser(string)
+	def test_tokenise(string):
+		tokens = tokenise(string)
 		for i, t in enumerate(tokens):
 			print('token {0} is {1}'.format(i, t))
 		print(tokens)
 
 
-	template = '''aaa{% include header.html %}
+	template = '''aaa{#comment#}aaa{% include header.html %}
 	<token id='profile'>
 	<h1>{{ person.name }}</h1>
 	<ul id='friends-list'>
@@ -77,5 +78,5 @@ if __name__ == "__main__":
 	{% include footer.html %}
 	<p>the end</p>'''
 
-	test_tokeniser(template)
+	test_tokenise(template)
 
