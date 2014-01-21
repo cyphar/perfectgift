d4682e1d69288d1ac720b0658cb325acc2e1c1c3
cyphar <cyphar@cyphar.com>
2014-01-21 19:57:42 +1100

    db: fixed up api

--- db/init.sql
+++ db/init.sql
@@ -2,31 +2,39 @@
 -- NOTE: run clean.sql *before* running this.
 
 CREATE TABLE tbl_users (
-	user_id INTEGER NOT NULL ,
-	fname TEXT NOT NULL,
-	lname TEXT NOT NULL,
-	username TEXT NOT NULL UNIQUE,
-	email TEXT NOT NULL UNIQUE,
-	image TEXT NOT NULL,
-	password TEXT NOT NULL,
-	salt TEXT NOT NULL,
-	dob TEXT,
+	user_id INTEGER NOT NULL,
+
+	fname TEXT NOT NULL, -- First name of user
+	lname TEXT NOT NULL, -- Last name of user
+
+	username TEXT NOT NULL UNIQUE, -- Username
+	email TEXT NOT NULL UNIQUE, -- Email address
+	image TEXT NOT NULL, -- Name of profile image (in /static/images/profile/*)
+
+	password TEXT NOT NULL, -- *Hashed* password
+	salt TEXT NOT NULL, -- Salt used for hash
+
+	dob INTEGER, -- Date of birth (epoch time)
 	PRIMARY	KEY (user_id)
 );
 
 CREATE TABLE tbl_products (
 	product_id INTEGER NOT NULL,
+	name TEXT NOT NULL,
+
 	image TEXT,
 	link TEXT,
-	name TEXT NOT NULL,
+
 	description TEXT,
-	price DOUBLE,
+	price TEXT,
+
 	PRIMARY KEY (product_id)
 );
 
 CREATE TABLE tbl_friends (
-	f_user_id INTEGER NOT NULL,
-	friend_id INTEGER NOT NULL,
+	f_user_id INTEGER NOT NULL, -- person *sending* friend request
+	friend_id INTEGER NOT NULL, -- person *recieving* friend request
+
 	PRIMARY KEY (f_user_id, friend_id),
 	FOREIGN KEY (friend_id) REFERENCES tbl_users,
 	FOREIGN KEY (f_user_id) REFERENCES tbl_users
@@ -36,6 +44,7 @@ CREATE TABLE tbl_wish_list (
 	wish_id INTEGER NOT NULL,
 	list_name TEXT NOT NULL,
 	user_id INTEGER NOT NULL,
+
 	PRIMARY KEY (wish_id),
 	FOREIGN KEY (user_id) REFERENCES tbl_users
 );
@@ -44,6 +53,7 @@ CREATE TABLE tbl_list_item (
 	list_id INTEGER NOT NULL,
 	product_id INTEGER NOT NULL,
 	checked INTEGER NOT NULL,
+
 	PRIMARY KEY (list_id, product_id),
 	FOREIGN KEY (product_id) REFERENCES tbl_products,
 	FOREIGN KEY (list_id) REFERENCES tbl_wish_list
--- db/password.py
+++ db/password.py
@@ -2,7 +2,6 @@ import hashlib
 import string
 import random
 
-availChars = string.ascii_letters + string.digits + '!#$%&\()*+,-./:;<=>?@[\\]^_`{|}~'
 users = {'Nick':('a60bd04d09d498fd5bd3f6b916045eaefd356737701f45aaee410ec76a19d340', '/Cv6<z^n'),   #is cool
 		 'Jon':('ece4f91a56ba8041965fddc79128ddf06c91332d6fea6a9062266f60e8e6cc50', '/_(pC]cW'),	#is 1337
 		 'admin':('41c158da53e3a3a577074f39a654258bafb59320e38416b5c531dc0ba78fcfc6', '*\\,fZ>W>')}  #admin
@@ -10,29 +9,37 @@ users = {'Nick':('a60bd04d09d498fd5bd3f6b916045eaefd356737701f45aaee410ec76a19d3
 #example password and salt generation:
 
 #Gemerates a random salt and returns it
-def saltGen():
-	return (''.join(random.choice(availChars) for i in range(8)))
+def generate_salt(size=8):
+	valid_chars = string.ascii_letters + string.digits + '!#$%&\()*+,-./:;<=>?@[\\]^_`{|}~'
+	return ''.join(random.choice(valid_chars) for i in range(size))
 
 
 #hashPW('is cool',saltGen()) returns the hash for is cool along with the random salt used:
 #ie	 hashPW('is cool',saltGen())
 #	   ('a60bd04d09d498fd5bd3f6b916045eaefd356737701f45aaee410ec76a19d340', '/Cv6<z^n')
-def hashPw(password, salt):
-	for i in range(7):
-		password = (hashlib.sha256(password.encode('utf-8') + salt.encode('utf-8'))).hexdigest()
-	return password, salt
+def hash_password(password, salt, rounds=7):
+	for i in range(rounds):
+		password = hashlib.sha256(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
 
+	return password
 
 #passcheck takes raw username and password as strings, and compares them to users dict
 #returns True or False for correct or wrong login
 #ie	 passCheck('Nick','is cool')
 #	   True
 #	   (duh)
-def passCheck(username, password):
+'''
+def check_password(username, password, rounds=1000):
 	if username in users:
+
 		salt = users[username][1]
-		for i in range(7):
-			password = (hashlib.sha256(password.encode('utf-8') + salt.encode('utf-8'))).hexdigest()
+		password = hash_password(users[username][0], )
+
+		for i in range(rounds):
+			password = hashlib.sha256(password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+
 		if password == users[username][0]:
 			return True
+
 	return False
+'''
